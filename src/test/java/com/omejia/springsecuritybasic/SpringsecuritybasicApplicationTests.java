6da9609fada8dfe9544f5a255596b611db147326
Oscar Mejia <camecac35@gmail.com>
2022-08-08 14:56:07 -0500

    first commit

---
+++ src/test/java/com/omejia/springsecuritybasic/SpringsecuritybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omejia.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
